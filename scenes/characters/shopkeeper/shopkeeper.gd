75e8c206efb73462ae98768cc6495a09a4cb5763
DenilsonDesigns <daniellanguilller@gmail.com>
2025-10-15 21:39:29 +1100

    shopkeeper moves without jitters

--- scenes/characters/shopkeeper/shopkeeper.gd
+++ scenes/characters/shopkeeper/shopkeeper.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var tend_target: Marker2D = $TendTarget
+@export var interaction_area: Area2D
 
 var move_speed := 60.0
 var moving := false
@@ -9,8 +10,15 @@ var start_position := Vector2.ZERO
 var target_position := Vector2.ZERO
 
 func _ready():
+	if interaction_area == null:
+		push_error("Shopkeeper missing InteractionArea reference! Please assign it in the Inspector.")
+		return
+	
 	start_position = global_position
 	target_position = global_position
+
+	interaction_area.body_entered.connect(_on_interaction_area_body_entered)
+	interaction_area.body_exited.connect(_on_interaction_area_body_exited)
 
 func _physics_process(_delta):
 	if moving:
@@ -18,7 +26,6 @@ func _physics_process(_delta):
 		velocity = direction * move_speed
 
 		_play_walk_animation(direction)
-
 		move_and_slide()
 
 		if global_position.distance_to(target_position) < 2.0:
@@ -26,18 +33,17 @@ func _physics_process(_delta):
 			velocity = Vector2.ZERO
 			_play_idle_animation(direction)
 
-func _on_interaction_area_body_entered(_body: Node2D) -> void:
-	# @TODO: for now, this wont do anything
-	# if body.name == "Player":
-	# 	body.show_interact_prompt("Talk")
-	print("entered")
-	target_position = tend_target.global_position
-	moving = true
+func _on_interaction_area_body_entered(body: Node2D) -> void:
+	if body.name == "Selan":
+		print("entered")
+		target_position = tend_target.global_position
+		moving = true
 
-func _on_interaction_area_body_exited(_body: Node2D) -> void:
-	print("exited")
-	target_position = start_position
-	moving = true
+func _on_interaction_area_body_exited(body: Node2D) -> void:
+	if body.name == "Selan":
+		print("exited")
+		target_position = start_position
+		moving = true
 
 func _play_walk_animation(direction: Vector2):
 	if abs(direction.x) > abs(direction.y):
@@ -52,7 +58,5 @@ func _play_idle_animation(direction: Vector2):
 		animation_player.play("idle_down" if direction.y > 0 else "idle_up")
 
 func interact():
-	# Called by the player when pressing "interact" inside InteractionArea
-	# @TODO: for now, wont be used.
 	target_position = tend_target.global_position
 	moving = true
