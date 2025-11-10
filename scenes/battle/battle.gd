3ea12a8fcc79cbd386f95ce077df6d9707bb9487
DenilsonDesigns <daniellanguilller@gmail.com>
2025-11-10 21:17:31 +1100

    basic player battle selection working

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var enemy_scenes: Array[PackedScene] = []
 @onready var enemy_container := $EnemyContainer
 @onready var player_stats_card := $PlayerStatsCard
+@onready var action_selection_card := $ActionSelectionCard
 @onready var hp_bar := $PlayerStatsCard/HPBar/BarFill
 @onready var mp_bar := $PlayerStatsCard/MPBar/BarFill
 
@@ -12,7 +13,20 @@ const MAX_X: float = 90.0
 const MIN_Y: float = -65.0
 const MAX_Y: float = -40.0
 
+enum BattleState {
+	IDLE,
+	PLAYER_TURN,
+	ENEMY_TURN,
+	RESOLVING,
+	VICTORY,
+	DEFEAT
+}
+
+var state: BattleState = BattleState.IDLE
+
 func _ready():
+	action_selection_card.action_selected.connect(_on_action_selected)
+
 	if background_texture_path == "":
 		push_error("Battle background not set!")
 		return
@@ -33,7 +47,56 @@ func setup(player_stats: Dictionary) -> void:
 		hp_bar.setup(player_stats["hp"], player_stats["max_hp"])
 	if mp_bar:
 		mp_bar.setup(player_stats["mp"], player_stats["max_mp"])
+	
+	start_battle()
 
+
+func start_battle():
+	print("Battle started!")
+	state = BattleState.PLAYER_TURN
+	show_player_action_select_menu(true)
+
+func show_player_action_select_menu(show_menu: bool) -> void:
+	action_selection_card.visible = show_menu
+	set_process(visible)
+
+func on_player_action_selected(action: String) -> void:
+	state = BattleState.RESOLVING
+	show_player_action_select_menu(false)
+	# @TODO: create method:
+	# resolve_player_action(action)
+
+func start_enemy_turn():
+	state = BattleState.ENEMY_TURN
+	print("Enemy is thinking...")
+	await get_tree().create_timer(1.0).timeout # fake delay
+	# @TODO: create method:
+	# resolve_enemy_action()
+
+func resolve_player_action(action: String):
+	print("Player used %s!" % action)
+	# TODO: Apply damage/heal/etc.
+	# @TODO: create method:
+	# check_battle_end()
+
+func resolve_enemy_action():
+	print("Enemy attacks!")
+	# TODO: Apply damage
+	# @TODO: create method:
+	# check_battle_end()
+
+func check_battle_end():
+	if GameState.get_player_stats()["hp"] <= 0:
+		state = BattleState.DEFEAT
+		print("You lose!")
+	# @TODO: create method:
+	# elif enemies_defeated():
+	# 	state = BattleState.VICTORY
+	# 	print("You win!")
+	else:
+		state = BattleState.PLAYER_TURN
+		# @TODO: create method:
+		show_player_action_select_menu(true)
 
 func _spawn_enemies():
 	var count: int = enemy_scenes.size()
@@ -55,3 +118,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"): # ESC key by default
 		print("Exiting battle, returning to field...")
 		LevelSwapper.return_from_battle()
+
+func _on_action_selected(action: String) -> void:
+	print("Player chose action: ", action)
+	show_player_action_select_menu(false)
+	resolve_player_action(action)
--- scenes/battle/ui/action_selection_card.gd
+++ scenes/battle/ui/action_selection_card.gd
@@ -4,8 +4,13 @@ extends PanelContainer
 @onready var defend_icon: AnimatedSprite2D = %DefendIcon
 @onready var item_icon: AnimatedSprite2D = %ItemIcon
 
+signal action_selected(action: String)
+
 var icons: Array[AnimatedSprite2D]
 var selected_index := 0
+
+# @TODO: dont like this:
+var actions := ["attack", "defend", "item"]
 
 func _ready():
 	icons = [attack_icon, defend_icon, item_icon]
@@ -18,14 +23,16 @@ func _process(_delta):
 			selected_index = 0
 		_update_icon_selection()
 	elif Input.is_action_just_pressed("ui_left"):
-		selected_index -= 1
-		if selected_index < 0:
-			selected_index = icons.size() - 1
+		selected_index = (selected_index - 1 + icons.size()) % icons.size()
 		_update_icon_selection()
+	elif Input.is_action_just_pressed("ui_accept"):
+		_emit_selected_action()
 
 func _update_icon_selection():
 	for i in range(icons.size()):
-		if i == selected_index:
-			icons[i].play("selected")
-		else:
-			icons[i].play("default")
+		icons[i].play("selected" if i == selected_index else "default")
+
+func _emit_selected_action():
+	var action = actions[selected_index]
+	print("Selected action: ", action)
+	emit_signal("action_selected", action)
